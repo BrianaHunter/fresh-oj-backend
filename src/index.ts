a2a7fc39ca7639c39d011e4339014cc286213755
Bianca Gjokaj <bianca.gjokaj@gmail.com>
2022-09-15 19:37:03 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { config } from "./config/config";
+// import { userRouter } from './routes/user.routes';
+// import { profileRouter } from './routes/profile.routes';
+// import { linkRouter } from './routes/link.routes';
+// import { authRouter } from './routes/auth.routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Routes
+// app.use('/auth', authRouter);
+// app.use('/users', userRouter);
+// app.use('/profiles', profileRouter);
+// app.use('/links', linkRouter);
+
+// Setup mongoose mongodb connection
+mongoose
+  .connect(config.mongoUri)
+  .then(() => console.log("Connected to MongoDB"));
+
+const port = config.port;
+
+app.listen(port, () => console.log(`Listening on port: ${port}.`));
